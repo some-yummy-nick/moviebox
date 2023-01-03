d5a88b33e147f940c16a2749ffac185aeeb623f7
some.yummy.nick <some.yummy.nick@gmail.com>
2023-01-03 13:55:14 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const { ObjectId } = require('mongodb')
+const { connectToDb, getDb } = require('./db')
+
+const PORT = 3000
+
+const app = express()
+
+const handleError = (res, error) => {
+  res.status(500).json({ error })
+}
+
+let db
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(PORT, (err) => {
+      err ? console.log(err) : console.log(`Listening port ${PORT}`)
+    })
+    db = getDb()
+  } else {
+    console.log(`DB connection error: ${err}`)
+  }
+})
+
+app.get('/movies', (req, res) => {
+  const movies = []
+  db.collection('movies')
+    .find()
+    .sort({ title: 1 })
+    .forEach((movie) => movies.push(movie))
+    .then(() => {
+      res.status(200).json(movies)
+    })
+    .catch(() => handleError(res, 'Something goes wrong...'))
+})
+
+app.get('/movies/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('movies')
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc)
+      })
+      .catch(() => handleError(res, 'Something goes wrong...'))
+  } else {
+    handleError(res, 'Wrong id')
+  }
+})
+
+app.delete('/movies/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('movies')
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result)
+      })
+      .catch(() => handleError(res, 'Something goes wrong...'))
+  } else {
+    handleError(res, 'Wrong id')
+  }
+})
