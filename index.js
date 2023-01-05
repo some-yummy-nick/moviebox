5049551c3cf78fa308a8451f3de21a45961ce25d
some.yummy.nick <some.yummy.nick@gmail.com>
2023-01-05 18:24:33 +0300

    add colors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const colors = require('colors')
 const PORT = process.env.PORT
 const DB_URL = process.env.DB_URL
+
+colors.enable()
 
 const app = express()
 
@@ -16,9 +19,9 @@ mongoose.set('strictQuery', false)
 
 mongoose
   .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log('Connected to MongoDB'.green))
   .catch((err) => console.log(`DB connection error: ${err}`))
 
 app.listen(PORT, (err) => {
-  err ? console.log(err) : console.log(`listening port ${PORT}`)
+  err ? console.log(err) : console.log(`listening port ${PORT}`.blue.bgCyan)
 })
